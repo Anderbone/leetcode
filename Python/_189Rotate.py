41ed16525aad01b07fffbba2767d1873c83b522b
Anderbone <yanchiyu@outlook.com>
2019-05-21 21:22:50 +0100

    array

--- Python/_189Rotate.py
+++ Python/_189Rotate.py
@@ -1,12 +1,19 @@
 from typing import List
+'''
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: [1,2,3,4,5,6,7] and k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+'''
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
 
-        # for i in range(k): # time exceed
-        #     last = nums[-1]
-        #     for j in range(len(nums)-1,0,-1):
-        #         nums[j] = nums[j-1]
-        #     nums[0] = last
         """
         Do not return anything, modify nums in-place instead.
         """
