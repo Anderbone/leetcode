da707edd23ccaa1d4ee5047bc6fb22f0c8231c07
Anderbone <yanchiyu@outlook.com>
2021-01-06 20:24:07 +0000

    2 problem

---
+++ Python/66.plus-one.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=66 lang=python3
+#
+# [66] Plus One
+#
+
+# @lc code=start
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in reversed(range(len(digits))):
+            if digits[i] != 9:
+                digits[i] += 1
+                return digits
+            else:
+                digits[i] = 0
+                if i == 0:
+                    digits.insert(0, 1)
+                    # digits[0] = 1
+                    # digits.append(0)
+        return digits
+        
+# @lc code=end
+
